d06775d64965412c6fa63d4ce357aeaca1f70f0a
konstantin <drondert@mail.ru>
2021-04-19 10:08:07 +0300

    completed lesson-4 homework

---
+++ utils.py
@@ -0,0 +1,13 @@
+import requests
+
+CURRENCY_API_URL = 'http://www.cbr.ru/scripts/XML_daily.asp'
+
+
+def currency_rates(char_code):
+    char_code = char_code.upper()
+    response = requests.get(CURRENCY_API_URL).text
+    try:
+        return float(response[response.find('<Value>', response.find(char_code)) + 7:response.find('</Value>',
+                              response.find(char_code))].replace(',', '.'))
+    except ValueError:
+        return None
